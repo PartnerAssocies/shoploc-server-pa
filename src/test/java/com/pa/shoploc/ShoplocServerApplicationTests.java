e2e3208b6acd8e3b8911338bab0c282ac2932965
Anthony BLIECQ <anthony.bliecq@gmail.com>
2020-10-12 18:38:45 +0200

    init serv

---
+++ src/test/java/com/pa/shoploc/ShoplocServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pa.shoploc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplocServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
