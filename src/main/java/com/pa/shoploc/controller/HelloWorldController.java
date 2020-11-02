27b5bb6d5e10621f880cca0c000f05e613558def
Théo DECLERCK <theo.declercks@hotmail.fr>
2020-11-02 21:23:20 +0100

    fix: fixed helloworld  controller
    fixed some conflicts

--- src/main/java/com/pa/shoploc/controller/HelloWorldController.java
+++ src/main/java/com/pa/shoploc/controller/HelloWorldController.java
@@ -1,19 +1,12 @@
 package com.pa.shoploc.controller;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
-=======
->>>>>>> dfa659aac134f8b9837f2437ca4cc7487d63234a
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-<<<<<<< HEAD
 @CrossOrigin("*")
-=======
-@CrossOrigin
->>>>>>> dfa659aac134f8b9837f2437ca4cc7487d63234a
 public class HelloWorldController {
 
 	@GetMapping("/")
